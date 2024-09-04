bd5321b33164ee6c53eb365eaeef28cba621f5bc
Gabriel Araujo da Silva Brito <rm558802@fiap.com.br>
2024-09-04 18:22:01 -0300

    Continuando formulário de cadastro.

--- mecanica-agil/src/Componentes/Cadastro/FormCadastro/index.tsx
+++ mecanica-agil/src/Componentes/Cadastro/FormCadastro/index.tsx
@@ -1,46 +1,54 @@
 import "./style.css";
-import logoMecanicaAgil from "../../../assets/img/logo.svg"; 
+import logoMecanicaAgil from "../../../assets/img/logo.svg";
 
 const FormCadastro = () => {
   return (
-    <form className="formCadastro" action="#" method="post">
-      <h1>CADASTRE-SE</h1>
+    <div className="boxFormCadastro">
+      <form className="formCadastro" action="#" method="post">
+        <h1>CADASTRE-SE</h1>
+        <p>Crie uma conta em nossa plataforma e acesse todos nossos recursos.</p>
+        <section className="camposFormCadastro">
+          <input placeholder="Digite seu nome..." type="text" id="nome" name="nome_completo" required />
+          <input placeholder="Digite seu e-mail..." type="email" id="email" name="email" required />
+          <input placeholder="Digite sua senha" type="password" id="senha" name="senha" required />
+        </section>
+        <label htmlFor="data-nascimento">Data de nascimento:</label>
+        <div className="birthdate">
+          <select id="dia" name="dia" required>
+            {[...Array(31)].map((_, i) => <option key={i} value={i + 1}>{i + 1}</option>)}
+          </select>
+          <select id="mes" name="mes" required>
+            {[...Array(12)].map((_, i) => <option key={i} value={i + 1}>{i + 1}</option>)}
+          </select>
+          <select id="ano" name="ano" required>
+            {[...Array(101)].map((_, i) => {
+              const currentYear = new Date().getFullYear();
+              return <option key={i} value={currentYear - i}>{currentYear - i}</option>
+            })}
+          </select>
+        </div>
+        <label htmlFor="genero">Genêro:</label>
+        {/* <div className="gender-options">
+          <label>
+            <input type="radio" name="genero" value="F" required /> F
+          </label>
+          <label>
+            <input type="radio" name="genero" value="M" required /> M
+          </label>
+          <label>
+            <input type="radio" name="genero" value="Outro" required /> Outro
+          </label>
+        </div> */}
 
-      <label htmlFor="nome">Nome Completo</label>
-      <input type="text" id="nome" name="nome_completo" required />
+        <button type="submit">Cadastra-se</button>
 
-      <label htmlFor="email">E-mail</label>
-      <input type="email" id="email" name="email" required />
+        <div className="logoFormCadastro">
+          <img src={logoMecanicaAgil} alt="Logo Mecânica Ágil" />
 
-      <label htmlFor="senha">Senha</label>
-      <input type="password" id="senha" name="senha" required />
+        </div>
+      </form>
+    </div>
 
-      <label htmlFor="data-nascimento">Data de nascimento:</label>
-      <div className="birthdate">
-        <select id="dia" name="dia" required></select>
-        <select id="mes" name="mes" required></select>
-        <select id="ano" name="ano" required></select>
-      </div>
-
-      <div className="gender-options">
-        <label>
-          <input type="radio" name="genero" value="F" required /> F
-        </label>
-        <label>
-          <input type="radio" name="genero" value="M" required /> M
-        </label>
-        <label>
-          <input type="radio" name="genero" value="Outro" required /> Outro
-        </label>
-      </div>
-
-      <button type="submit">Cadastra-se</button>
-
-      <div className="logo">
-        <img src={logoMecanicaAgil} alt="Logo Mecânica Ágil" />
-        <div className="logo-text">MECÂNICA ÁGIL</div>
-      </div>
-    </form>
   );
 };
 
