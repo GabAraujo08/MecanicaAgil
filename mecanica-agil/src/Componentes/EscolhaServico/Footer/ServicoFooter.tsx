5bf216e80a5649e36293035f47c7d75f2ca5bce4
Thiago Henry Dias <thiago.henrydias@gmail.com>
2024-09-08 18:15:33 -0300

    Footer

---
+++ mecanica-agil/src/Componentes/EscolhaServico/Footer/ServicoFooter.tsx
@@ -0,0 +1,37 @@
+import '../Footer/style.css'
+
+
+const ServicoFooter = () => {
+    return (
+        <footer>
+            <div className="conteudo-footer">
+                <nav>
+                    <ul>
+                        <li>Produtos</li>
+                        <li>Nossa equipe</li>
+                        <li>Trabalhe conosco</li>
+                        <li>Seja um parceiro</li>
+                    </ul>
+                </nav>
+                <nav>
+                    <ul>
+                        <li>Informações</li>
+                        <li>SAC</li>
+                        <li>Blog</li>
+                        <li>Suporte</li>
+                    </ul>
+                </nav>
+                <nav>
+                    <ul>
+                        <li>Nosso negócio</li>
+                        <li>Sobre nós</li>
+                        <li>Cultura</li>
+                        <li>Entre em contato</li>
+                    </ul>
+                </nav>
+            </div>
+        </footer>
+    );
+}
+
+export default ServicoFooter;
--- mecanica-agil/src/EscolhaServicoPage.tsx
+++ mecanica-agil/src/EscolhaServicoPage.tsx
@@ -1,16 +1,22 @@
 
 import './App.css'
 import ServicoBanner from './Componentes/EscolhaServico/Banner/ServicoBanner';
+import ServicoFooter from './Componentes/EscolhaServico/Footer/ServicoFooter';
 import ServicoHeader from './Componentes/EscolhaServico/Header/EscolhaServico';
 import OpcaoServico from './Componentes/EscolhaServico/Main/Opcao';
 
 function EscolhaServicoPage() {
     return (
         <main className='global'>
-        <ServicoHeader />
-        <ServicoBanner />
-        <OpcaoServico />
+            <ServicoHeader />
+            <ServicoBanner />
+            <OpcaoServico />
+        <section className='margem'>
+            <ServicoFooter />
+        </section>
         </main>
+        
+       
 
         
     )
