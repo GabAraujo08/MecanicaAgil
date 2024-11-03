adbcd1a5d7b8befbbc79ae6d90293b11981316af
Gabriel Araujo da Silva Brito <rm558802@fiap.com.br>
2024-11-03 13:15:20 -0300

    Update index.tsx

--- mecanica-agil-nextjs/src/app/Componentes/TabelaMecanica/index.tsx
+++ mecanica-agil-nextjs/src/app/Componentes/TabelaMecanica/index.tsx
@@ -20,7 +20,7 @@ const TabelaMecanicas: React.FC = () => {
   useEffect(() => {
     const fetchMecanicas = async () => {
       try {
-        const response = await fetch('http://meuprojeto.link/mecanica-agil/api/mecanica/all');
+        const response = await fetch('/api/proxy/mecanica/all'); // Mudou para usar o proxy
         if (!response.ok) {
           throw new Error('Erro ao buscar dados das mecânicas');
         }
@@ -31,9 +31,10 @@ const TabelaMecanicas: React.FC = () => {
         console.error(error);
       }
     };
-
+  
     fetchMecanicas();
   }, []);
+  
 
   const handleDelete = async (id: number) => {
     try {
